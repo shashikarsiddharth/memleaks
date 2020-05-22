6fd6f7e9862cb419d39872a49134e63b3a28886c
shashikarsiddharth <siddharth.shashikar@trustingsocial.com>
2020-05-22 15:48:13 +0530

    added break statement

--- kill.sh
+++ kill.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # -> Get docker container id using xargs // sudo docker ps | grep "engine_worker" | awk '{ print $1 }'
 # -> While True:
@@ -15,9 +16,11 @@ CONTAINER_ID=$(sudo docker ps | grep "memleaks" | awk '{ print $1 }')
 while true 
 do
     VAL=$(sudo docker stats ${CONTAINER_ID} --no-stream | awk '{ print $4 }' | tail -1 | tr -d 'MiB' | awk '{print int($1)}')
-    if [ $VAL -gt 50 ]
+    if [ $VAL -gt 10 ]
     then
-        `sudo docker stop ${CONTAINER_ID}`
+        # container name, mem consumption, time, exit code, logging. 
+        sudo docker stop ${CONTAINER_ID}
         break
     fi
+    sleep 5s
 done
